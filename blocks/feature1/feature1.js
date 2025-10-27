2b81710e9123d722fb06f33fec0eee24df8e2907
Rich Greenland <rpg300@gmail.com>
2025-10-27 15:13:18 -0400

    Added feature1card

--- blocks/feature1/feature1.js
+++ blocks/feature1/feature1.js
@@ -2,6 +2,6 @@ export default function decorate(block) {
     const [quoteWrapper] = block.children;
   
     const blockquote = document.createElement('blockquote');
-    blockquote.textContent = quoteWrapper.textContent.trim();
+    blockquote.textContent = quoteWrapper.textContent.trim() + "hHHeeyy";
     quoteWrapper.replaceChildren(blockquote);
   }
