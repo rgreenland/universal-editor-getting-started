110a989e429dde4380a1f92d5af6e6d003ca8fd6
Rich Greenland <rpg300@gmail.com>
2025-10-27 15:54:52 -0400

    Added feature1card

--- blocks/feature1/feature1.js
+++ blocks/feature1/feature1.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
-    const [quoteWrapper] = block.children;
+    const [feature1Wrapper] = block.children;
   
-    const blockquote = document.createElement('blockquote');
+    const blockfeature1 = document.createElement('blockfeature1');
      //blockquote.textContent = quoteWrapper.textContent.trim() + "hHHeeyy";
-     blockquote.textContent = "";
-     blockquote.setHTMLUnsafe("<div class=\"MuiGrid-root MuiGrid-container css-1w5a2c\" data-fondue-recipe=\"feature-2:1.0.0\" data-fondue-version=\"2.31.0\">hey</div>")
+     blockfeature1.textContent = "";
+     blockfeature1.setHTMLUnsafe("<div class=\"MuiGrid-root MuiGrid-container css-1w5a2c\" data-fondue-recipe=\"feature-2:1.0.0\" data-fondue-version=\"2.31.0\">hey</div>")
     //  blockquote.textContent += "<div class=\"MuiGrid-root MuiGrid-container css-1w5a2c\" data-fondue-recipe=\"feature-2:1.0.0\" data-fondue-version=\"2.31.0\">";
     //  blockquote.textContent += "hey</div>";
-    quoteWrapper.replaceChildren(blockquote);
+    quoteWrapper.replaceChildren(blockfeature1);
   }
