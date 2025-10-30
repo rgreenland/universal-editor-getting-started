0a9841697479f6ee258200e6413760c9e5eda66f
Rich Greenland <rpg300@gmail.com>
2025-10-30 14:58:16 -0400

    Fixed it so it stop outputted everything

--- blocks/feature1/feature1.js
+++ blocks/feature1/feature1.js
@@ -22,9 +22,11 @@ export default function decorate(block) {
     divCardContainer.className="css-l33vzl";
     [...block.children].forEach((card, index) => {
     Cards.push(new Card(card.children[0].innerText, card.children[1].innerText, card.children[2].innerText, card.children[3].innerText, card.children[4].innerText, card.children[5].innerText, card.children[6].innerText, card.children[7].innerText, card.children[8].innerText));
+
+  
     const divCard1 = document.createElement('div');
     divCard1.className = "MuiGrid-root MuiGrid-item MuiGrid-grid-mobile-4 MuiGrid-grid-tablet-4 MuiGrid-grid-desktop-3 css-swicca";
-    
+    moveInstrumentation(row, divCard1);
     const divCard2 = document.createElement('div');
     divCard2.className = "MuiBox-root css-azi59w";
     
